8e2d52a398bc361695ad8f2ef97508ef3c74369d
richforest0506 <richforest0506@gmail.com>
2021-01-16 20:19:00 +0500

    clear

--- src/app/pages/home/CreateToken/utils.js
+++
@@ -1,6 +0,0 @@
-export default class Utils {
-  static async importFromPublic(fileName: string) {
-    const response = await fetch(process.env.PUBLIC_URL + fileName)
-    return response.text()
-  }
-}
--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export default{
-    firebaseFunctionUrl:"https://us-central1-hivedb-cdbf7.cloudfunctions.net/"
-}
