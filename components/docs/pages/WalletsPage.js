24739755d9f8afd1ad2957a5f5f2afc58d06f574
richforest0506 <richforest0506@gmail.com>
2021-06-20 17:32:05 +0530

    balance docs updated

--- components/docs/pages/WalletsPage.js
+++ components/docs/pages/WalletsPage.js
@@ -194,22 +194,13 @@ function WalletsPage(){
             </div>
             < CodeContainer title = {`
     authToken: <your auth token>
-    type: <your wallet type>        //type value should be BSV or STAS and by default type is BSV
+    currency: <which type of balance you want to get>           //defalt value is "USD".,
+    walletsID: [ <array string of wallets id> ]                 // default value is ["vaionex-wallet"]
 `}
             />
             <div style={styles.paraSpace}>
                 <p>To get balance, you have to pass two header parameters one is <b>authToken</b> which is required and second <b>type</b>. type key value should be <b>BSV</b> or <b>STAS</b></p>
             </div>
-
-
-            <div style={styles.heading2Space}>
-                <h6>Body</h6>
-            </div>
-            < CodeContainer title = {`{
-    "walletId": ["<your wallet id>"]            // array string of your wallet id
-}
-`}
-            />
 
              <div style={styles.paraSpace}>
                 <p>To get balance, you have to pass array string of your wallet id to body and key will be <b>walletId</b> </p>
@@ -226,11 +217,8 @@ function WalletsPage(){
         headers: {
             Content-Type: "application/json,
             authToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFiMGNiMTk5Zjg3MGYyOGUyOTg5YWI0ODFjYzJlNDdlMGUyY2MxOWQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdmFpb25leGRldiIsImF1ZCI6InZhaW9uZXhkZXYiLCJhdXRoX3RpbWUiOjE2MjMwODEyOTYsInVzZXJfaWQiOiJ6Yk0yeDNVRXVEUzFiZWNlbGhGWTJvTzhnNkQyIiwic3ViIjoiemJNMngzVUV1RFMxYmVjZWxoRlkyb084ZzZEMiIsImlhdCI6MTYyMzA4MTI5NiwiZXhwIjoxNjIzMDg0ODk2LCJlbWFpbCI6IjEyMzVAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbIjEyMzVAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.ZgRVxWH--tJdVuBdzjl2NeABWW605z0kEub37n3nEbWQzu1BlxNV6I_MOtUXfv1g6QOzc6mgK7gpQvkGGXgprxqv0tOuaPGUjVUQKemPLbt4bWdf-fGWbAekH-4k4TQZqpI2vyfhDclc_G9B0eh7_83jE0H19KPLTj42Lmqzr5SLEIpijf5AlHOPK-9QUaQCuQo-VVSM6h2XP4jOXruZQ7VacMTiJYWHxxX2FELHGoTBiWImNQrBfPL3OXOWhq9yDwaxBxVNJuBAyGDGRp6K9zPK5YsZKKFXQBpzP5sLxg1ekUTa7u6tprWiHqa1ODyBoS2FozIWb5m8-luP6BKXIQ",
-            type: "BSV"
+            currency: "BSV"
         },
-        data:{
-            walletId: ["vaionex-wallet"]
-        }
     };
 
     axios(config).then((res) =>{
@@ -251,12 +239,26 @@ function WalletsPage(){
 
             <CodeContainer title={
                 `{
-    "message": "Operation completed successfully",
-    "statusCode": 200,
-    "data": {
-        "status": "success",
-        "data": []              // if you are getting empty data array means you don't have balance.
+  "statusCode": 200,
+  "data": {
+    "status": "success",
+    "msg": "Operation completed successfully",
+    "stasTokenBal": {
+      "data": [
+        "string"
+      ]
+    },
+    "otherBal": {
+      "data": {
+        "walletData": "string",
+        "totalBalance": {
+          "bsvBal": 619338,
+          "currencyCode": "BSV",
+          "currencyBal": 619338
+        }
+      }
     }
+  }
 }
                 `
             }/>
