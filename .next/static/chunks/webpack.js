00a455794a57781b2cc1257040386b9a142e3fc8
richforest0506 <richforest0506@gmail.com>
2021-08-03 21:39:35 +0300

    fix global-scale section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d0acdb9a9ef43da94b9b";
+/******/ 	var hotCurrentHash = "2a769ed108147657a9ac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
