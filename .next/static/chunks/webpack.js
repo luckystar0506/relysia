59f0d2e75e35c7b8ee98383e5c12ea3b893dc9a8
richforest0506 <richforest0506@gmail.com>
2021-08-10 19:38:52 +0300

    fixed wrong class name

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4bb7bd2805689358377c";
+/******/ 	var hotCurrentHash = "449bb705d92268bd1da1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
